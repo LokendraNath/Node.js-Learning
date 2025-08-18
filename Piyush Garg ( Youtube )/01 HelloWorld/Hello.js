f5cbc0a60381bbcff04acdd3a31bf62542288000
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-18 10:31:01 +0530

    learn about modules

--- Piyush Garg ( Youtube )/01 HelloWorld/Hello.js
+++ Piyush Garg ( Youtube )/01 HelloWorld/Hello.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+import { addFunc, subFunc } from "./Math.js";
+
+
+console.log("Hello World");
+console.log(addFunc(5, 3));
+console.log(subFunc(5, 3));
---
+++ Piyush Garg ( Youtube )/01 HelloWorld/math.js
@@ -0,0 +1,6 @@
+export const addFunc = (a, b) => {
+  return a + b;
+};
+export const subFunc = (a, b) => {
+  return a - b;
+};
