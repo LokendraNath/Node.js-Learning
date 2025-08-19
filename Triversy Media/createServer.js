adce77e709bedcf0b000947e3f5a2ee81a3c6029
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-19 16:33:07 +0530

    simple routing

--- Triversy Media/createServer.js
+++ Triversy Media/createServer.js
@@ -1,11 +1,25 @@
 import http from "http";
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(req.method);
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello world</h1>");
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>I Am Lokendra Nath</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method Not Allowed");
+    }
+  } catch (error) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Server Error");
+  }
 });
 
 server.listen(PORT, () => {
