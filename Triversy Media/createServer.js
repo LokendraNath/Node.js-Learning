91a3374b3ff0f67ec5b532d9dbfefeafe2fcd7ad
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-19 14:21:29 +0530

    .gitignore and .env files

--- Triversy Media/createServer.js
+++ Triversy Media/createServer.js
@@ -1,8 +1,8 @@
 import http from "http";
-const PORT = 8000;
+const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type",'text/html')
-  res.end("<h1>hello world</h1>");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello world</h1>");
 });
 
 server.listen(PORT, () => {
