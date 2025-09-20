175c3df49186ca29534e73f27b5189e305556087
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-20 18:14:37 +0530

    make folder clean

--- Piyush Garg ( Youtube )/01 Basics/hello.js
+++
@@ -1,6 +0,0 @@
-import { addFunc, subFunc } from "./Math.js";
-
-
-console.log("Hello World");
-console.log(addFunc(5, 3));
-console.log(subFunc(5, 3));
--- Piyush Garg ( Youtube )/01 Basics/math.js
+++
@@ -1,6 +0,0 @@
-export const addFunc = (a, b) => {
-  return a + b;
-};
-export const subFunc = (a, b) => {
-  return a - b;
-};
